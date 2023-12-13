fd3bf7eedd1f1f494c8ec0616cc8b6599b0d7597
Your Name <you@example.com>
2023-12-13 17:33:06 +0100

    error overlay design

--- components/UI/ErrorOverlay.js
+++ components/UI/ErrorOverlay.js
@@ -1,8 +1,35 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function ErrorOverlay() {
+function ErrorOverlay({ message, onError }) {
   return (
-    <div>ErrorOverlay</div>
-  )
+    <View style={styles.container}>
+      <Text style={styles.title}>An error occured!</Text>
+      <Text style={styles.text}>{message}</Text>
+      <Button title="Okay!" onPress={onError} />
+    </View>
+  );
 }
 
-export default ErrorOverlay
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 8,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 12,
+    margin: 6,
+  },
+});
+
+export default ErrorOverlay;
